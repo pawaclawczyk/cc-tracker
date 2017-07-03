effda757357179ab37737cf4f532bb35997608e8
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2017-07-03 22:04:54 +0200

    Add saving request to file.

--- src/aerys.php
+++ src/aerys.php
@@ -9,10 +9,22 @@ use Aerys\Request;
 use Aerys\Response;
 use function Aerys\router;
 use Amp\File;
+use function Amp\File\open;
+
+$log = open(__DIR__.'/../var/log/requests.log', 'a+');
+
+$storeRequest = function (array $headers) use ($log) {
+    $log->when(function ($err, $handle) use ($headers) {
+        $handle->write(json_encode($headers));
+    });
+};
 
 $router = router()
-    ->get("/pixel.gif", function (Request $request, Response $response) {
+    ->get("/pixel.gif", function (Request $request, Response $response) use ($storeRequest) {
+        $storeRequest($request->getAllHeaders());
+
         $pixel = yield File\get(__DIR__.'/../var/static/pixel.gif');
+
         $response
             ->addHeader('Content-Type', 'image/gif')
             ->end($pixel);
