75f19d6408d63f63cc7da3ec32fe082bd3d4f290
Paweł Wacławczyk <p.waclawczyk@clearcode.cc>
2017-07-05 12:43:13 +0200

    Add RabbitMQ to docker-compose. Raw implementation for publishing messages in message queue.

--- src/CC/Tracker/Controller/PixelController.php
+++ src/CC/Tracker/Controller/PixelController.php
@@ -8,6 +8,9 @@ use Aerys\Request;
 use Aerys\Response;
 use Amp\File;
 use Amp\Promise;
+use Amp\ReactAdapter\ReactAdapter;
+use Bunny\Async\Client;
+use Bunny\Channel;
 use SebastianBergmann\GlobalState\RuntimeException;
 
 final class PixelController
@@ -23,7 +26,10 @@ final class PixelController
             ->addHeader('Content-Type', 'image/gif')
             ->end($pixel);
 
-        $this->store($request);
+        $data = $this->prepareData($request);
+
+        $this->send($data);
+        $this->store($data);
     }
 
     private function open(): Promise
@@ -35,10 +41,13 @@ final class PixelController
         return $this->handle;
     }
 
-    private function store(Request $request)
+    private function prepareData(Request $request): string
     {
-        $data = json_encode(array_merge($request->getAllHeaders(), $request->getAllParams()));
+        return json_encode(array_merge($request->getAllHeaders(), $request->getAllParams()));
+    }
 
+    private function store(string $data)
+    {
         $this->open()->onResolve(function ($error, File\Handle $handle) use ($data) {
             if ($error) {
                 throw new RuntimeException($error);
@@ -46,5 +55,28 @@ final class PixelController
 
             $handle->write($data);
         });
+    }
+
+    private function send(string $data)
+    {
+        $connection = [
+            'host'      => 'rabbit',
+            'user'      => 'rabbit',
+            'password'  => 'rabbit.123',
+        ];
+
+        (new Client(ReactAdapter::get(), $connection))
+            ->connect()
+            ->then(function (Client $client) {
+                return $client->channel();
+            })
+            ->then(function (Channel $channel) {
+                $channel->queueDeclare('tracker');
+
+                return $channel;
+            })
+            ->then(function (Channel $channel) use ($data) {
+                $channel->publish($data, [], '', 'tracker');
+            });
     }
 }
